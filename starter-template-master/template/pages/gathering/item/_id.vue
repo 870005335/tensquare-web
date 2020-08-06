befbbb4f4bf0be3ed10cd17928095c4136d73b84
liubin <870005335@qq.com>
2020-08-06 18:33:22 +0800

    first commit

---
+++ starter-template-master/template/pages/gathering/item/_id.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        活动详情
+        {{item.id}}
+        <hr>
+        {{item.name}}
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    asyncData({params}) {
+        return axios.get(`http://192.168.1.200:7300/mock/5f103bcfc6bd9c5b6e264bf6/tensquare/gathering/gathering/${params.id}`)
+        .then(res => {
+            return {item: res.data.data}
+        })
+    }
+}
+</script>
